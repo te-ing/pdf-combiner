1bd373850123f0fe7723ce5bb3391b83eb0a1430
te-ing <hi2177@naver.com>
2025-08-18 21:30:23 +0900

    feat: 테스트시 모킹된 ipcRenderer 로직을 사용하도록 수정

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,16 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
+beforeAll(() => {
+  (window as any).electron = {
+    ipcRenderer: {
+      sendMessage: jest.fn(),
+      on: jest.fn().mockImplementation(() => () => {}),
+      once: jest.fn(),
+    },
+  };
+});
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
